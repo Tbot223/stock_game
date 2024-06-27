280ab24ba85c28d09f0f6937e388e11ba04feb95
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2024-06-27 21:29:57 +0900

    test

--- main.py
+++ main.py
@@ -2,5 +2,29 @@ Version = 0.1
 
 from main_systems import db
 
-db.db_check()
+import sys
+from PyQt5.QtWidgets import *
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300, 300, 400, 300)
+
+        btn1 = QPushButton("login")
+        btn2 = QPushButton("sing up")
+
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.addWidget(btn1)
+        layout.addWidget(btn2)
+        self.setCentralWidget(widget)
+
+        vbox = widget.findChildren(QVBoxLayout)[0]
+        btn = vbox.itemAt(0).widget()
+        print(btn.text())
+
+
+app = QApplication(sys.argv)
+window = MyWindow()
+window.show()
+app.exec_()
--- test2.py
+++ test2.py
@@ -4,17 +4,19 @@ from PyQt5.QtWidgets import *
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300, 300, 420, 120)
+        self.setGeometry(300, 300, 400, 300)
 
-        btn = QPushButton(text="로그인", parent=self)
-        btn.move(220, 10)
-        btn.resize(200, 100)
-        btn = QPushButton(text="가입", parent=self)
-        btn.move(0, 10)
-        btn.resize(200, 100)
+        btn1 = QPushButton("login")
+        btn2 = QPushButton("sing up")
 
+        widget = QWidget()
+        layout = QVBoxLayout(widget)
+        layout.addWidget(btn1)
+        layout.addWidget(btn2)
+        self.setCentralWidget(widget)
 
-app = QApplication(sys.argv)
-window = MyWindow()
-window.show()
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec_()
